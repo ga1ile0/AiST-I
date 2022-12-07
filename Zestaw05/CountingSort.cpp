6bafb8c2b7ea7b4c6a2ef72c63e100b09741db99
g1ile0 <dawid.wllosek@gmail.com>
2022-12-07 19:36:43 +0100

    Dopisany CountingSort

---
+++ Zestaw05/CountingSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+template<class T> vois CountingSort(std::vector<T>& v){
+    vector<int> count(max + 1, 0);
+    vector<T> output(v.size(), 0);
+    int max = v[0];
+
+    for(int i = 1 ; i < v.size(); ++i){
+        if(v[i] > max){
+            max = v[i];
+        }
+    }
+
+    for(int i=1; i < v.size(); ++i){
+        count[v[i]]++;
+    }
+
+    for(int i = 1 ; i <=max ; ++i){
+        count[i] += count[i-1];
+    }
+
+    for(int i = v.size() - 1; i >= 0; --i){
+        output[count[v[i]] - 1] = array[i];
+        count[v[i]]--;
+    }
+
+    for(int i = 0 ; i < v.size(); ++i){
+        v[i] == output[i];
+    }
+}
+
+int main(){
+    int x;
+    std::vector<int> v;
+    while(std::cin >> x)
+        v.push_back(x);
+    CountingSort<int>(v);
+    for(const auto& i : v)
+        std::cout << i << std::endl;
+}
