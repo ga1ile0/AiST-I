0314444d1c16c29e6457c73e97bb0ff4ad29b43e
g1ile0 <dawid.wllosek@gmail.com>
2022-12-08 23:48:20 +0100

    Dodanie komentarzy

--- Zestaw04/CursorList.hpp
+++ Zestaw04/CursorList.hpp
@@ -2,7 +2,7 @@
 #define CURSOR_LIST_H
 
 #include <iostream>
-#define DEF_SIZE 10000
+#define DEFcount 10000
 
 template<class T>
 class CursorList{
@@ -15,7 +15,7 @@ class CursorList{
         int head;
         int tail;
         int spare;
-        int _size;
+        int count;
         int capacity;
     public:
         CursorList();
@@ -38,15 +38,15 @@ class CursorList{
 
 template<class T>
 CursorList<T>::CursorList(){
-    array = new Node[DEF_SIZE];
-    capacity = DEF_SIZE;
+    array = new Node[DEFcount];
+    capacity = DEFcount;
     for(int i = 0; i < capacity; i++){
         array[i].next = i+1;
     }
     head = 0;
     tail = 0;
     spare = 0;
-    _size = 0;
+    count = 0;
 }
 
 template<class T>
@@ -59,18 +59,18 @@ CursorList<T>::CursorList(int size){
     head = 0;
     tail = 0;
     spare = 0;
-    _size = 0;
+    count = 0;
 }
 
 template<class T>
 CursorList<T>::~CursorList(){
     delete[] array;
-    _size = 0;
+    count = 0;
 }
 
 template<class T>
 void CursorList<T>::push_front(T x){
-    if(_size == capacity)
+    if(count == capacity)
         throw std::out_of_range("FULL");
     int temp = head;
     head = spare; //nowy element bedzie w miejscu spare i zostanie nowa glowa
@@ -78,7 +78,7 @@ void CursorList<T>::push_front(T x){
     spare = array[spare].next; //wyznaczam nowego spare'a
     array[head].next = temp; //przypinam poprzedni pierwszy element jako next nowej głowy
 
-    _size++;
+    count++;
 
 }
 
@@ -90,15 +90,15 @@ T CursorList<T>::pop_front(){
     head = array[head].next; //przesuwam głowe na drugi element 
     array[temp].next = spare; //'usuniety' element ma wskazywac na miejsce w ktorym przed usunieciem znajdowal sie spare
     spare = temp; //ustawiam pierwsze wolne miejsce na miejsce usunietego elementu; teraz next spare wskazuje na inny wolny element (ten usuniety)
-    _size--;
-    if(_size == 0) //jesli lista jest pusta to head ma byc w tym samym miejscu w tablicy co spare
+    count--;
+    if(count == 0) //jesli lista jest pusta to head ma byc w tym samym miejscu w tablicy co spare
         head = spare;
     return array[temp].value;
 }
 
 template<class T>
 void CursorList<T>::push_back(T x){
-    if(_size == capacity)
+    if(count == capacity)
         throw std::out_of_range("FULL");
     int temp = tail;
     tail = spare; //przesuwam tail na pierwsze wolne miejsce
@@ -106,7 +106,7 @@ void CursorList<T>::push_back(T x){
     spare = array[spare].next; //wyznaczam nowego spare'a
     array[temp].next = tail; //ustawiam next'a poprzedniego taila na nowego taila
 
-    _size++;
+    count++;
     
 }
 
@@ -115,39 +115,39 @@ T CursorList<T>::pop_back(){
     if(empty())
         throw std::out_of_range("EMPTY");
     int temp = head;
-    for(int i = 0; i < _size - 2; ++i){
+    for(int i = 0; i < count - 2; ++i){
         temp = array[temp].next;  //ustawiam temp na element 'przed' tail'em
     }
     array[tail].next = spare; //usuniety element ma wskazywac na miejsce w ktorym przed usunieciem znajdowal sie spare
     spare = tail; //ustawiam spare na miejsce usunietego elementy (dzieki linijce wyzej ten element juz wskazuje na inne wolne miejsce)
     tail = temp; //ustawiam tail na element 'wczesniejszy'
-    _size--;
+    count--;
     return array[spare].value;
 }
 
 template<class T>
 int CursorList<T>::size(){
-    return _size;
+    return count;
 }
 
 template<class T>
 bool CursorList<T>::empty(){
-    return (_size == 0);
+    return (count == 0);
 }
 
 template<class T>
 void CursorList<T>::clear(){
-    for(int i=0; i< _size; ++i){
+    for(int i=0; i< count; ++i){
         array[i].value = 0; //
         array[i].next = array[i+1];
     }
-    _size = 0;
+    count = 0;
 }
 
 template<class T>
 int CursorList<T>::find(T x){
     int temp = head;
-    for(int i = 0; i < _size; ++i){
+    for(int i = 0; i < count; ++i){
         if(array[temp].value == x)
             return i;
         temp = array[temp].next;
@@ -162,19 +162,19 @@ T CursorList<T>::erase(int i){ //deletes and returns i-th element
     int temp = head;
     for(int n = 0; n < i - 1; ++n){
         temp = array[temp].next;
-    } //now temp holds index of (i-1)-th element
-    int prev_spare = spare;
+    } //temp przechowuje indeks (i-1)-go elementu
+    int prev_spare = spare; //zapisuje indeks spare sprzed usunięcia elementu
     spare = array[temp].next; //spare ustawiam w miejsce usunietego elementu
     array[array[temp].next].next = prev_spare;//usuniety element ma wskazywac na poprzedniego spare
     array[temp].next = array[array[temp].next].next; //element przed usunietym ma wskazywac na element za usunietym
-    _size--;
+    count--;
     return array[spare].value; //zwracam wartosc usunietego elementu
 
 }
 
 template<class T>
 void CursorList<T>::insert(int i, T x){
-    if(_size == capacity)
+    if(count == capacity)
         throw std::out_of_range("FULL");
     int temp = head;
     for(int n = 0; n < i-2 ; n++){
@@ -199,7 +199,7 @@ int CursorList<T>::remove(T x){
             spare = array[temp].next;
             array[array[temp].next].next = prev_spare;
             array[temp].next = array[array[temp].next].next;
-            _size--;
+            count--;
         }
         temp = array[temp].next;
     }
@@ -209,7 +209,7 @@ int CursorList<T>::remove(T x){
 template<class T>
 bool CursorList<T>::replace(T x, T y){
     int temp = head;
-    for(int i = 0; i < _size; ++i){
+    for(int i = 0; i < count; ++i){
         if(array[temp].value == x){
             array[temp].value = y;
             return true;
